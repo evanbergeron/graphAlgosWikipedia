ba6be1bf4f290589d4d92d23a5ada5eaa68736e1
Evan Bergeron <bergeronej@gmail.com>
2014-11-18 00:14:16 -0500

    First commit

---
+++ dfs_demo.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+start = "http://en.wikipedia.org/wiki/Mandarin_Chinese"
+end = "http://en.wikipedia.org/wiki/Thomas_the_Tank_Engine"
+
+def getLinks(url):
+    webpage = urllib2.urlopen(url)
+    soup = BeautifulSoup(webpage)
+    prefix = "http://en.wikipedia.org"
+    return [prefix + l["href"] for l in soup.findAll("a", href=True)
+            if l["href"].startswith("/wiki")]
+
+def djk(start, end):
+    visited = [start]
+    curr = start
+    toVisit = getLinks(start)
+    while toVisit != []:
+        curr = toVisit.pop(0)
+        visited.append(curr)
+        print curr
+        if curr == end:
+            return end
+        toVisit = [link for link in getLinks(curr) if link not in visited] + toVisit
+
+# print djk(start, end)
+print "\n".join(getLinks(start))
